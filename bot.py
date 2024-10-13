6ccf1e96315f1473ba21c5f4dc2ab7e4310f7893
kakarotmuiinfinity <166479138+kakarotmuiinfinity@users.noreply.github.com>
2024-10-13 06:33:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,6 @@
+from config import *
 import telebot
+import openai
 
-Token = "7712183768:AAFB_5sDCNLNKcMwhvyjLP-nSFqkmSkiXkI"
 
-bot = telebot.TeleBot(Token)
-
-@bot.message_handler(['start'])
-def start(message):
-       bot.reply_to(message, "Hᴇʟʟᴏ {username}✨ Mʏsᴇʟғ {bot_name} Wᴀɴᴛ ᴛᴏ ᴡᴀᴛᴄʜ Aɴɪᴍᴇ? I ᴄᴀɴ ᴘʀᴏᴠɪᴅᴇ ʏᴏᴜ Aɴɪᴍᴇ ʏᴏᴜ ᴡᴀɴᴛ!")
-    
-bot.polling()
+bot = telebot.TeleBot(BOT_API)
